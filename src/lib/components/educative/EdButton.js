597da1ae2cd7689897c2ed157978892894d8b485
Marigameo <mariappangameo@gmail.com>
2021-08-02 23:51:13 +0530

    default value added for edButton

--- src/lib/components/educative/EdButton.js
+++ src/lib/components/educative/EdButton.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './style.module.css'
 
-export default function EdButton({type, children}) {
+export default function EdButton({type = 'outlined', children}) {
     return (
         <button className={`${styles.btn} ${styles[`btn_${type}`]}`}>{children}</button>
     )
